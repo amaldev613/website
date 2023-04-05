ca75e3ee8166145976738e68750cfdef55c645f2
amaldev613 <129602896+amaldev613@users.noreply.github.com>
2023-04-05 08:11:48 +0000

    job portal

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, jsonify
 
-app = Flask(__name__)   
+app = Flask(__name__)
+
+JOBS = [{
+  'id': 1,
+  'company': 'SwipeWire',
+  'title': 'Data Analyst',
+  'location': 'Bengaluru, India',
+  'salary': 'Rs. 10,00,000'
+}, {
+  'id': 2,
+  'company': 'Kiddily',
+  'title': 'Data Scientist',
+  'location': 'Delhi, India',
+  'salary': 'Rs. 15,00,000'
+}, {
+  'id': 3,
+  'company': 'Bandcamp',
+  'title': 'Frontend Engineer',
+  'location': 'Mumbai, India',
+  'salary': 'Rs. 12,00,000'
+}, {
+  'id': 4,
+  'company': 'Formonix',
+  'title': 'Backend Engineer',
+  'location': 'Kochi, India',
+  'salary': 'Rs. 10,00,000'
+}]
+
 
 @app.route("/")
-def hello_world():
-  return render_template("home.html")
+def hello_jovian():
+  return render_template('home.html', jobs=JOBS)
 
-if __name__=="__main__":
-  app.run(host="0.0.0.0",debug=True)
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
